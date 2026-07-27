032edb671d6c60e9b195ff692e4acc63f7fcfd98
fais9210 <fais9210@gmail.com>
2026-07-28 01:51:45 +0700

    ubah drizle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,13 @@
-import { defineConfig } from 'drizzle-kit';
-import * as dotenv from 'dotenv';
+import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
-  out: './drizzle',
-  dialect: 'postgresql',
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
   dbCredentials: {
-    host: process.env.SQL_HOST || 'localhost',
-    port: process.env.SQL_PORT ? Number(process.env.SQL_PORT) : 5432,
-    user: process.env.SQL_USER || 'postgres',
-    password: process.env.SQL_PASSWORD || '',
-    database: process.env.SQL_DB_NAME || 'madrasah_db',
-    ssl: false,
+    // Membaca langsung dari DATABASE_URL di file .env
+    url: process.env.DATABASE_URL as string,
   },
 });
