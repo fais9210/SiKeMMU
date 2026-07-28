8a698ea6793e40b9aefb67a24636cbad8965c0ea
fais9210 <fais9210@gmail.com>
2026-07-28 15:06:33 +0700

    konfigurasi database supabase

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    host: process.env.SQL_HOST || 'localhost',
-    port: process.env.SQL_PORT ? Number(process.env.SQL_PORT) : 5432,
-    user: process.env.SQL_USER || 'postgres',
-    password: process.env.SQL_PASSWORD || '',
-    database: process.env.SQL_DB_NAME || 'madrasah_db',
-    ssl: false,
+    url: process.env.DATABASE_URL as string,
   },
 });
