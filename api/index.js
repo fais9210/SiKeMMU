ebc750f36c9a533445b10157254ed6bbd26be0e9
fais9210 <fais9210@gmail.com>
2026-07-29 12:45:08 +0700

    index

--- api/index.js
+++ api/index.js
@@ -1,3 +1,18 @@
-const server = require('../dist/server.cjs');
+export default async function handler(req, res) {
+  try {
+    const serverModule = await import('../dist/server.cjs');
+    const handlerFn = serverModule.default?.default || serverModule.default || serverModule;
 
-module.exports = server;
+    if (typeof handlerFn !== 'function') {
+      res.statusCode = 500;
+      res.end('Server handler not found');
+      return;
+    }
+
+    return handlerFn(req, res);
+  } catch (error) {
+    console.error('Vercel handler error:', error);
+    res.statusCode = 500;
+    res.end('Internal Server Error');
+  }
+}
